c78df4b193891520ad2554356a5a6879047159c7
Alex <alex.podikoglou@gmail.com>
2025-12-31 21:16:00 +0200

    style: run formatter

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,31 +2,31 @@ import type { FC } from "hono/jsx";
 import type { Context } from "../context";
 
 export const Navbar: FC<{ context: Context }> = ({ context }) => {
-  return (
-    <nav>
-      <div>
-        <li>
-          <a href="/">LPDB</a>
-        </li>
-      </div>
+	return (
+		<nav>
+			<div>
+				<li>
+					<a href="/">LPDB</a>
+				</li>
+			</div>
 
-      <div>
-        {context.userId ? (
-          <>
-            <li>
-              <a href="/submit">submit</a>
-            </li>
+			<div>
+				{context.userId ? (
+					<>
+						<li>
+							<a href="/submit">submit</a>
+						</li>
 
-            <li>
-              <a href="/auth/logout">log out</a>
-            </li>
-          </>
-        ) : (
-          <li>
-            <a href="/auth/login">log in</a>
-          </li>
-        )}
-      </div>
-    </nav>
-  );
+						<li>
+							<a href="/auth/logout">log out</a>
+						</li>
+					</>
+				) : (
+					<li>
+						<a href="/auth/login">log in</a>
+					</li>
+				)}
+			</div>
+		</nav>
+	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import type { FC } from "hono/jsx";
-import { Layout } from "../layout";
 import { Navbar } from "../components/navbar";
 import type { Context } from "../context";
+import { Layout } from "../layout";
 
 export const IndexPage: FC<{ context: Context }> = ({ context }) => {
-  return (
-    <Layout>
-      <Navbar context={context} />
+	return (
+		<Layout>
+			<Navbar context={context} />
 
-      <h1>Alex's license plates database</h1>
-      <p>
-        Here you can find a database of all the license plates I've spotted over
-        the past few years.
-      </p>
-    </Layout>
-  );
+			<h1>Alex's license plates database</h1>
+			<p>
+				Here you can find a database of all the license plates I've spotted over
+				the past few years.
+			</p>
+		</Layout>
+	);
 };

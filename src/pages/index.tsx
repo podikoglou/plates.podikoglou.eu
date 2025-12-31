3b45019a00a7d39a8fea93b42333f54f11ca0564
Alex <alex.podikoglou@gmail.com>
2025-12-31 21:29:09 +0200

    refactor: capitalize h1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export const IndexPage: FC = () => {
 		<Layout>
 			<Navbar />
 
-			<h1>Alex's license plates database</h1>
+			<h1>Alex's License Plates Database</h1>
 			<p>
 				Here you can find a database of all the license plates I've spotted over
 				the past few years.

4d3fb0665328bc42de01ff4a91e2f4b19a674193
Alex <alex.podikoglou@gmail.com>
2026-01-01 16:14:04 +0200

    perf: prepare entry query

--- src/pages/entry.tsx
+++ src/pages/entry.tsx
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { css, cx } from "hono/css";
 import type { FC } from "hono/jsx";
 import { Aside } from "../components/aside";
@@ -9,13 +9,15 @@ import { Navbar } from "../components/navbar";
 import { db } from "../db";
 import { entriesTable } from "../db/schema";
 
+const entryQuery = db
+	.select()
+	.from(entriesTable)
+	.where(eq(entriesTable.id, sql.placeholder("id")))
+	.limit(1)
+	.prepare();
+
 export const EntryPage: FC<{ entryId: number }> = async ({ entryId }) => {
-	const results = await db
-		.select()
-		.from(entriesTable)
-		.where(eq(entriesTable.id, entryId))
-		.limit(1)
-		.execute();
+	const results = await entryQuery.execute({ id: entryId });
 
 	if (results.length === 0) {
 		// TODO: 404

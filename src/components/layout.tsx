8e1b28b14e732b944bd43a36d3e158d9f155a5d5
Alex <alex.podikoglou@gmail.com>
2026-01-01 18:46:46 +0200

    feat: htmx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,6 +8,7 @@ export const Layout: FC = (props) => {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="stylesheet" href="/static/style.css" />
 				<link rel="stylesheet" href="/static/recursive.css" />
+				<script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js"></script>
 				<Style />
 			</head>
 			<body>{props.children}</body>

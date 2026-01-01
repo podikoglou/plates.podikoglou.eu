a0cb7035b902501df3e7eb14fbb145af71d3f107
Alex <alex.podikoglou@gmail.com>
2026-01-01 13:43:40 +0200

    feat: flag display

--- src/components/entries-table.tsx
+++ src/components/entries-table.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import { css } from "hono/css";
 import type { FC } from "hono/jsx";
 import type { entriesTable } from "../db/schema";
+import { plateCountryToCountryCode, plateCountryToFlagUrl } from "../flags";
 
 export const EntriesTable: FC<{
 	entries: (typeof entriesTable.$inferSelect)[];
@@ -34,7 +35,20 @@ export const EntriesTable: FC<{
 						<td class={thClass}>
 							<a href={`/entry/${entry.id}`}>#{entry.id}</a>
 						</td>
-						<td class={thClass}>{entry.countryCode ?? "-"}</td>
+						<td class={thClass}>
+							{entry.countryCode ? (
+								<>
+									{plateCountryToCountryCode[entry.countryCode] ? (
+										<img
+											src={plateCountryToFlagUrl(entry.countryCode)}
+											alt="flag"
+										/>
+									) : null}
+
+									{entry.countryCode}
+								</>
+							) : null}
+						</td>
 						<td class={thClass}>{entry.text ?? "-"}</td>
 						<td class={thClass}>
 							{entry.spottedOn ? format(entry.spottedOn, "MMM yyyy") : ""}
---
+++ src/flags.ts
@@ -0,0 +1,48 @@
+export const plateCountryToCountryCode: Record<string, string> = {
+	D: "DE",
+	NMK: "MK",
+	NL: "NL",
+	SRB: "RS",
+	CH: "CH",
+	EST: "EE",
+	UK: "GB",
+	H: "HU",
+	GB: "GB",
+	A: "AT",
+	PL: "PL",
+	TR: "TR",
+	RO: "RO",
+	F: "FR",
+	I: "IT",
+	AL: "AL",
+	BG: "BG",
+	// SL: "",
+	MD: "MD",
+	B: "BE",
+	BY: "BY",
+	US: "US",
+	GR: "GR",
+	UA: "UA",
+	MO: "MO",
+	DK: "DK",
+	CZ: "CX",
+	L: "LU",
+	S: "SE",
+	// GE: "",
+	// DE: "",
+	CY: "CY",
+	// GT: "",
+	BIH: "BA",
+	N: "NO",
+	FI: "FI",
+};
+
+export const plateCountryToFlagUrl = (
+	plateCountry: string,
+): string | undefined => {
+	const countryCode = plateCountryToCountryCode[plateCountry];
+
+	return countryCode
+		? `https://flagsapi.com/${countryCode}/flat/16.png`
+		: undefined;
+};
